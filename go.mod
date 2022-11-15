e09617df3c393070e7e955a2f26d4e07675994b6
Wei <windos0901@gmail.com>
2022-11-15 17:34:39 +0800

    fix

--- go.mod
+++ go.mod
@@ -3,6 +3,9 @@ module www
 go 1.14
 
 require (
-	github.com/julienschmidt/httprouter v1.3.0
-	github.com/streadway/amqp v1.0.0
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/streadway/amqp v1.0.0 // indirect
+	github.com/ugorji/go/codec v1.2.7
+	go.mongodb.org/mongo-driver v1.10.2 // indirect
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22 // indirect
 )
--- main.go
+++ main.go
@@ -1,51 +1,5 @@
 package main
 
-import (
-	"encoding/json"
-	"fmt"
-	"time"
-
-	"github.com/streadway/amqp"
-)
-
 func main() {
-	// test
-	conn, err := amqp.Dial("amqp://root:abcd1234!@localhost:5672/")
-	if err != nil {
-		fmt.Printf("%s: %s\n", "Failed to connect to RabbitMQ", err)
-	}
 
-	ch, err := conn.Channel()
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer ch.Close()
-
-	msgs, err := ch.Consume(
-		"history", // queue
-		"",        // consumer
-		true,      // Auto Ack
-		false,     // exclusive
-		false,     // no-local
-		false,     // no-wait
-		nil,       // args
-	)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	forever := make(chan bool)
-	type List struct {
-		GameId string `json:"game_id"`
-		Num    string `json:"num"`
-	}
-	go func() {
-		for d := range msgs {
-			list := &List{}
-			json.Unmarshal([]byte(d.Body), &list)
-			fmt.Println("時間：" + time.Now().Format("2006-01-02 15:04:05") + "\t| 遊戲名稱：" + list.GameId + "\t| 期數：" + list.Num)
-		}
-	}()
-	fmt.Println(" [*] Waiting for messages. To exit press CTRL+C")
-	<-forever
 }
